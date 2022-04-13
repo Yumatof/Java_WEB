ea939da316a218da66116431e8ea307bf27ce866
Yumatov Maksim <yumatof@ya.ru>
2022-04-13 17:33:34 +0300

    HW4 minor update +

--- src/main/java/org/example/HW4/MyException/NonExistentTriangle.java
+++ src/main/java/org/example/HW4/MyException/NonExistentTriangle.java
@@ -1,4 +1,8 @@
 package org.example.HW4.MyException;
 
 public class NonExistentTriangle extends Exception{
+    public NonExistentTriangle(String errorMsg){
+        super(errorMsg);
+    }
+
 }
--- src/main/java/org/example/HW4/TriangleSquare.java
+++ src/main/java/org/example/HW4/TriangleSquare.java
@@ -1,21 +1,30 @@
 package org.example.HW4;
 
+import org.example.HW4.MyException.NonExistentTriangle;
 import static java.lang.Math.sqrt;
 
 public class TriangleSquare {
-
+    private final String INCORRECT_SIDE_TRIANGLE = "Triangle with given sides exists.";
 
     public double calculationSquare(int sideA, int sideB, int sideC){
 
-
+        try {
+            checkSideTriangle(sideA,sideB,sideC);
+        } catch (NonExistentTriangle e){
+            e.printStackTrace();
+        }
 
 
         int halfPerimeter = (sideA + sideB + sideC)/2;
         return sqrt(halfPerimeter * (halfPerimeter - sideA) * (halfPerimeter - sideB) * (halfPerimeter - sideC));
     }
 
-    private void checkSideTriangle(int sideA, int sideB, int sideC){
-
+    private void checkSideTriangle(int sideA, int sideB, int sideC) throws NonExistentTriangle {
+        if(((sideA + sideB) > sideC)
+                &&((sideA + sideC) > sideB)
+                &&((sideB + sideC) > sideA)) {
+            throw new NonExistentTriangle(INCORRECT_SIDE_TRIANGLE);
+        }
     }
 
 
